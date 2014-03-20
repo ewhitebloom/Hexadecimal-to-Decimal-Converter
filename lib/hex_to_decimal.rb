87c48e56e00caaa483e47666caa5c078ef932390
ewhitebloom <ewhitebloom@gmail.com>
2014-03-20 13:34:29 -0400

    indents

--- lib/hex_to_decimal.rb
+++ lib/hex_to_decimal.rb
@@ -1,19 +1,19 @@
 def hex_to_decimal(hex)
-conversions = []
-hex.each_char do |character|
-  if character.match(/^[[:alpha:]]$/).nil?
-    conversions << character.to_i
-  else
-   character.upcase!
-   ref = { 'A' => 10, 'B' => 11, 'C' => 12, 'D' => 13, 'E' => 14, 'F' => 15 }
-   conversions << ref[character]
+  conversions = []
+  hex.each_char do |character|
+    if character.match(/^[[:alpha:]]$/).nil?
+      conversions << character.to_i
+    else
+     character.upcase!
+     ref = { 'A' => 10, 'B' => 11, 'C' => 12, 'D' => 13, 'E' => 14, 'F' => 15 }
+     conversions << ref[character]
+    end
   end
-end
-final = 0
-incrementer = conversions.length - 1
-conversions.each do |conversion|
-  final += (conversion * (16**incrementer))
-  incrementer -= 1
-end
-final
+  final = 0
+  incrementer = conversions.length - 1
+  conversions.each do |conversion|
+    final += (conversion * (16**incrementer))
+    incrementer -= 1
+  end
+  final
 end
