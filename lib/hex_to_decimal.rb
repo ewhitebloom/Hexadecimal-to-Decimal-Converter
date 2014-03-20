20c018efe6365016a614d1482ea3c74b1526ab1e
ewhitebloom <ewhitebloom@gmail.com>
2014-03-20 13:28:58 -0400

    rubocop changes

--- lib/hex_to_decimal.rb
+++ lib/hex_to_decimal.rb
@@ -1,16 +1,19 @@
 def hex_to_decimal(hex)
-  decimal = []
-  hex.each_char do |character|
-    if character.match(/^[[:alpha:]]$/) == nil
-      decimal << character.to_i
-    else
-     character.upcase!
-     ref = {"A"=>10,"B"=>11,"C"=>12,"D"=>13,"E"=>14,"F"=>15}
-     decimal << ref[character]
-    end
-  end
-  final = 0
-  incrementer = decimal.length - 1
-  decimal.each { |conversion| final += (conversion * (16**incrementer)); incrementer -= 1 }
-  final
+decimal = []
+hex.each_char do |character|
+  if character.match(/^[[:alpha:]]$/).nil?
+    decimal << character.to_i
+  else
+   character.upcase!
+   ref = { 'A' => 10, 'B' => 11, 'C' => 12, 'D' => 13, 'E' => 14, 'F' => 15 }
+   decimal << ref[character]
+ end
+end
+final = 0
+incrementer = decimal.length - 1
+decimal.each do |conversion|
+  final += (conversion * (16**incrementer))
+  incrementer -= 1
+end
+final
 end
