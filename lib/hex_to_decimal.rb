3293238f49180c05dde9d11cc34961de1e7ba423
ewhitebloom <ewhitebloom@gmail.com>
2014-03-20 12:58:54 -0400

    lints fixed

--- lib/hex_to_decimal.rb
+++ lib/hex_to_decimal.rb
@@ -1,12 +1,12 @@
 def hex_to_decimal(hex)
   decimal = []
-  hex.each_char do |hex|
-    if hex.match(/^[[:alpha:]]$/) == nil
-      decimal << hex.to_i
+  hex.each_char do |character|
+    if character.match(/^[[:alpha:]]$/) == nil
+      decimal << character.to_i
     else
-     hex.upcase!
+     character.upcase!
      ref = {"A"=>10,"B"=>11,"C"=>12,"D"=>13,"E"=>14,"F"=>15}
-     decimal << ref[hex].to_i
+     decimal << ref[character].to_i
     end
   end
   final = 0
